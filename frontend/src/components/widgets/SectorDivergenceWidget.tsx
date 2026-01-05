42747c2c37bb99b3b7e6f264f4345c79b1a803fa
Steven Meyer <stevenmeyer@macbookpro.lan>
2026-01-05 09:04:23 -0500

    Fix duplicate SVG closing tag in SectorDivergenceWidget

--- frontend/src/components/widgets/SectorDivergenceWidget.tsx
+++ frontend/src/components/widgets/SectorDivergenceWidget.tsx
@@ -317,7 +317,6 @@ export default function SectorDivergenceWidget({ trendPeriod = 90 }: Props) {
             );
           })}
         </svg>
-        </svg>
       </div>
 
       {/* Expandable Sector Details */}
